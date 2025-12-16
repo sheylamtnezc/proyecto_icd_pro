72b3625bcd2c3b6a8e65f6947db00d0206bd6db6
sheyla mtnez <sheylamtnezc@gmail.com>
2025-12-16 14:08:06 -0500

    delete files

--- codigos/min y max.py
+++
@@ -1,8 +0,0 @@
-def min_max(lista):
-    if not lista:
-        return None, None
-    return min(lista), max(lista)   
-
-print(min_max([10,20,30,40,50]))
-print(min_max([]))
-print(min_max([5]))
--- codigos/promedio.py
+++
@@ -1,7 +0,0 @@
-def promedio(lista):
-    if not lista:
-        return 0
-    return sum(lista) / len(lista)
-
-print(promedio([10,20,30,40,50]))
-print(promedio([]))
